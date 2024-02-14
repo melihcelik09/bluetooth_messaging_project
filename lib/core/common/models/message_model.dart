c14b04b240dc1797bdd0258cbaf1661fab0a15ce
Melih Çelik <melihcelik@izmir.bel.tr>
2024-02-14 10:07:13 +0300

    Update senderId to be nullable in MessageModel

--- lib/core/common/models/message_model.dart
+++ lib/core/common/models/message_model.dart
@@ -1,12 +1,12 @@
 import 'package:bluetooth_messaging_project/core/enum/message_type.dart';
 
 class MessageModel {
-  final String senderId;
+  String? senderId;
   final String content;
   final MessageType type;
 
   MessageModel({
-    required this.senderId,
+    this.senderId,
     required this.content,
     required this.type,
   });
