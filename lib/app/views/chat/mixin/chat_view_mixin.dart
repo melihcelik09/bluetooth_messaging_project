9c1fb6723aa3f8fdc955ada2cd694e83078ea998
Melih Çelik <melihcelik@izmir.bel.tr>
2024-02-15 15:07:57 +0300

    Add ChatViewMixin for chat functionality

---
+++ lib/app/views/chat/mixin/chat_view_mixin.dart
@@ -0,0 +1,163 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:bluetooth_messaging_project/app/views/chat/view/chat_view.dart';
+import 'package:bluetooth_messaging_project/core/common/models/message_model.dart';
+import 'package:bluetooth_messaging_project/core/enum/message_type.dart';
+import 'package:bluetooth_messaging_project/core/extension/string_extension.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:record/record.dart';
+
+mixin ChatViewMixin on State<ChatView> {
+  late StreamSubscription _receivedDataSubscription;
+  List<MessageModel> messages = [];
+  late TextEditingController controller;
+  late AudioRecorder recorder;
+  late StreamSubscription<Amplitude?> _amplitudeSubscription;
+  final List<double> amplitudes = [];
+  late AudioPlayer player;
+  late Duration position;
+  bool isRecording = false;
+  bool isPlaying = false;
+  String? audioPath;
+  Duration? maxDuration;
+  Timer? _timer;
+  late Duration recordDuration;
+  late ScrollController noiseController;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController();
+    noiseController = ScrollController();
+    recorder = AudioRecorder();
+    player = AudioPlayer();
+    position = const Duration();
+    recordDuration = const Duration();
+
+    _amplitudeSubscription = recorder
+        .onAmplitudeChanged(const Duration(milliseconds: 300))
+        .listen((amp) {
+      setState(() {
+        /// The line `double scaled = (1 - (amp.current.abs() / 50)).clamp(0.0, 100.0) * 100;` is
+        /// calculating the scaled amplitude value based on the current amplitude value received from
+        /// the microphone.
+        double scaled = (1 - (amp.current.abs() / 50)).clamp(0.05, 100.0) * 100;
+        amplitudes.add(scaled);
+        noiseController.animateTo(
+          noiseController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      });
+    });
+
+    player.onDurationChanged.listen((event) {
+      setState(() {
+        maxDuration = event;
+      });
+    });
+    player.onPositionChanged.listen((event) {
+      if (event == maxDuration) {
+        setState(() {
+          isPlaying = false;
+        });
+      } else {
+        setState(() {
+          position = event;
+        });
+      }
+    });
+    _receivedDataSubscription = widget.service.dataReceivedSubscription(
+      callback: (data) {
+        MessageModel message = MessageModel(
+          senderId: data["senderDeviceId"],
+          content: data["message"],
+          type: MessageType.receiver,
+        );
+        setState(() {
+          messages.add(message);
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _receivedDataSubscription.cancel();
+    recorder.dispose();
+    _amplitudeSubscription.cancel();
+    player.dispose();
+  }
+
+  Future<void> recordAudio() async {
+    try {
+      if (await recorder.hasPermission()) {
+        var appDirectory = await getApplicationDocumentsDirectory();
+        await recorder.start(
+          const RecordConfig(),
+          path: "${appDirectory.path}/recording.m4a",
+        );
+        _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+          setState(() {
+            recordDuration = recordDuration + const Duration(seconds: 1);
+          });
+        });
+        setState(() {
+          isRecording = true;
+        });
+      }
+    } catch (e) {
+      debugPrint("Recording Error: $e");
+    }
+  }
+
+  Future<void> stopRecording() async {
+    try {
+      String? path = await recorder.stop();
+      _timer?.cancel();
+      debugPrint("Record Path: $path");
+      setState(() {
+        isRecording = false;
+        audioPath = path;
+      });
+    } catch (e) {
+      debugPrint("Stopping Record Error: $e");
+    }
+  }
+
+  Future<void> playRecording() async {
+    try {
+      if (isPlaying) {
+        await player.pause();
+        setState(() {
+          isPlaying = false;
+        });
+      } else {
+        File file = File(audioPath!);
+        if (await file.exists()) {
+          DeviceFileSource local = DeviceFileSource(audioPath!);
+          await player.play(local);
+          setState(() {
+            isPlaying = true;
+          });
+        } else {
+          debugPrint("File not found");
+        }
+      }
+    } catch (e) {
+      debugPrint("Playing Record Error: $e");
+    }
+  }
+
+  String getDuration() {
+    if (isPlaying) {
+      return position.format;
+    } else {
+      return recordDuration.format;
+    }
+  }
+}
--- lib/app/views/chat/view/chat_view.dart
+++ lib/app/views/chat/view/chat_view.dart
@@ -1,17 +1,14 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:audioplayers/audioplayers.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:bluetooth_messaging_project/app/views/chat/mixin/chat_view_mixin.dart';
 import 'package:bluetooth_messaging_project/app/views/chat/view/widgets/chat_bubble.dart';
+import 'package:bluetooth_messaging_project/app/views/chat/view/widgets/play_sound.dart';
+import 'package:bluetooth_messaging_project/app/views/chat/view/widgets/record_sound.dart';
 import 'package:bluetooth_messaging_project/core/common/common_text_form_field.dart';
 import 'package:bluetooth_messaging_project/core/common/models/message_model.dart';
 import 'package:bluetooth_messaging_project/core/enum/chat_type.dart';
 import 'package:bluetooth_messaging_project/core/enum/message_type.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_nearby_connections/flutter_nearby_connections.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:record/record.dart';
 
 @RoutePage()
 class ChatView extends StatefulWidget {
@@ -29,137 +26,7 @@ class ChatView extends StatefulWidget {
   State<ChatView> createState() => _ChatViewState();
 }
 
-class _ChatViewState extends State<ChatView> {
-  late StreamSubscription receivedDataSubscription;
-  List<MessageModel> messages = [];
-  late TextEditingController _controller;
-  late AudioRecorder _recorder;
-  late AudioPlayer _player;
-  late Duration _position;
-  bool isRecording = false;
-  bool isPlaying = false;
-  String? audioPath;
-  Duration? maxDuration;
-  Timer? _timer;
-  late Duration _recordDuration;
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController();
-    _recorder = AudioRecorder();
-    _player = AudioPlayer();
-    _position = const Duration();
-    _recordDuration = const Duration();
-    _player.onDurationChanged.listen((event) {
-      setState(() {
-        maxDuration = event;
-      });
-    });
-    _player.onPositionChanged.listen((event) {
-      if (event == maxDuration) {
-        setState(() {
-          isPlaying = false;
-        });
-      } else {
-        setState(() {
-          _position = event;
-        });
-      }
-    });
-    receivedDataSubscription = widget.service.dataReceivedSubscription(
-      callback: (data) {
-        MessageModel message = MessageModel(
-          senderId: data["senderDeviceId"],
-          content: data["message"],
-          type: MessageType.receiver,
-        );
-        setState(() {
-          messages.add(message);
-        });
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    receivedDataSubscription.cancel();
-    _recorder.dispose();
-    _player.dispose();
-  }
-
-  Future<void> recordAudio() async {
-    try {
-      if (await _recorder.hasPermission()) {
-        var appDirectory = await getApplicationDocumentsDirectory();
-        await _recorder.start(
-          const RecordConfig(),
-          path: "${appDirectory.path}/recording.m4a",
-        );
-        _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-          setState(() {
-            _recordDuration = _recordDuration + const Duration(seconds: 1);
-          });
-        });
-        setState(() {
-          isRecording = true;
-        });
-      }
-    } catch (e) {
-      debugPrint("Recording Error: $e");
-    }
-  }
-
-  Future<void> stopRecording() async {
-    try {
-      String? path = await _recorder.stop();
-      _timer?.cancel();
-      debugPrint("Record Path: $path");
-      setState(() {
-        isRecording = false;
-        audioPath = path;
-      });
-    } catch (e) {
-      debugPrint("Stopping Record Error: $e");
-    }
-  }
-
-  Future<void> playRecording() async {
-    try {
-      if (isPlaying) {
-        await _player.pause();
-        setState(() {
-          isPlaying = false;
-        });
-      } else {
-        File file = File(audioPath!);
-        if (await file.exists()) {
-          DeviceFileSource local = DeviceFileSource(audioPath!);
-          await _player.play(local);
-          setState(() {
-            isPlaying = true;
-          });
-        } else {
-          debugPrint("File not found");
-        }
-      }
-    } catch (e) {
-      debugPrint("Playing Record Error: $e");
-    }
-  }
-
-  String getDuration() {
-    if (isPlaying) {
-      return formatDuration(_position);
-    } else {
-      return formatDuration(_recordDuration);
-    }
-  }
-
-  String formatDuration(Duration duration) {
-    return "${duration.inMinutes}:${duration.inSeconds.remainder(60).toString().padLeft(2, '0')}";
-  }
-
+class _ChatViewState extends State<ChatView> with ChatViewMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -181,40 +48,27 @@ class _ChatViewState extends State<ChatView> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             (!isRecording && audioPath != null)
-                ? ChatInputCard(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(getDuration()),
-                      Slider(
-                        inactiveColor: Colors.grey,
-                        value: _position.inSeconds.toDouble(),
-                        onChanged: (value) async {
-                          await _player.seek(Duration(seconds: value.toInt()));
-                        },
-                        min: 0,
-                        max: maxDuration?.inSeconds.toDouble() ?? 0,
-                      ),
-                      IconButton(
-                        onPressed: playRecording,
-                        icon: Icon(isPlaying ? Icons.stop : Icons.play_arrow),
-                      ),
-                    ],
+                ? PlaySound(
+                    duration: getDuration(),
+                    position: position,
+                    maxDuration: maxDuration ?? recordDuration,
+                    isPlaying: isPlaying,
+                    player: player,
+                    playRecording: playRecording,
                   )
                 : isRecording
-                    ? ChatInputCard(
-                        children: [
-                          Text(
-                            getDuration(),
-                          ),
-                        ],
+                    ? RecordSound(
+                        duration: getDuration(),
+                        noises: amplitudes,
+                        controller: noiseController,
                       )
                     : Expanded(
                         child: CommonTextFormField(
                           hintText: 'Type a message',
-                          controller: _controller,
+                          controller: controller,
                           onChanged: (value) {
                             setState(() {
-                              _controller.text = value;
+                              controller.text = value;
                             });
                           },
                         ),
@@ -227,26 +81,26 @@ class _ChatViewState extends State<ChatView> {
                 : const SizedBox.shrink(),
             IconButton(
               onPressed: () async {
-                if (_controller.text == "/clear") {
+                if (controller.text == "/clear") {
                   setState(() {
                     messages.clear();
-                    _controller.clear();
+                    controller.clear();
                   });
                   return;
                 }
                 MessageModel message = MessageModel(
-                  content: _controller.text,
+                  content: controller.text,
                   type: MessageType.sender,
                 );
                 for (Device device in widget.devices) {
                   await widget.service.sendMessage(
                     device.deviceId,
-                    _controller.text,
+                    controller.text,
                   );
                 }
                 setState(() {
                   messages.add(message);
-                  _controller.clear();
+                  controller.clear();
                 });
               },
               icon: const Icon(Icons.send),
@@ -270,34 +124,6 @@ class _ChatViewState extends State<ChatView> {
               ),
             );
           },
-        ),
-      ),
-    );
-  }
-}
-
-class ChatInputCard extends StatelessWidget {
-  final List<Widget> children;
-  final MainAxisAlignment mainAxisAlignment;
-  const ChatInputCard({
-    super.key,
-    required this.children,
-    this.mainAxisAlignment = MainAxisAlignment.start,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        height: kToolbarHeight,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.grey[300],
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          mainAxisAlignment: mainAxisAlignment,
-          children: children,
         ),
       ),
     );
